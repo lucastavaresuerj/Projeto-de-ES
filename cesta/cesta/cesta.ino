bc8f3fd35db84507bb0422bdb420a45afb35c974
lucastavaresuerj <lucastavaresuerj2016@gmail.com>
2019-10-20 17:46:59 -0300

    prototipo e inicio a cesta e placar

---
+++ cesta/cesta/cesta.ino
@@ -0,0 +1,82 @@
+//cesta
+#include<SoftwareSerial.h>
+#include "Ultrasonic.h"
+#include "notas.h"
+#define txA 11
+#define rxA 10
+#define txB 6
+#define rxB 5
+#define switchbtn 2
+#define buzzer 3
+#define trig 8
+#define echo 9
+
+Ultrasonic ultrasonic(trig,echo); //INICIALIZANDO OS PINOS
+
+int estado = 0;
+int distancia;
+int btn = HIGH;
+
+SoftwareSerial HC12A(txA,rxA);
+SoftwareSerial HC12B(txB,rxB);
+
+void setup() {
+  HC12A.begin(9600);
+  HC12B.begin(9600);
+  Serial.begin(9600);
+  pinMode(buzzer, OUTPUT);
+  pinMode(switchbtn, INPUT);
+  pinMode(echo, INPUT); //DEFINE O PINO COMO ENTRADA (RECEBE)
+  pinMode(trig, OUTPUT); //DEFINE O PINO COMO SAÍDA (ENVIA)
+  Ultrasonic ultrasonic(trig,echo); //INICIALIZANDO OS PINOS
+}
+
+void loop() {
+    Serial.print("estado: ");Serial.println(estado);
+    switch(estado) {
+      case 0:
+        hcsr04();
+        if(distancia < 10) {
+          Serial.println("cesta A");
+          HC12A.write(49);
+          tone(buzzer,NOTE_C3,1000/4);
+          noTone(buzzer);
+        }
+        else if(!digitalRead(switchbtn) && btn) {
+          btn = LOW;
+        } 
+        else if(digitalRead(switchbtn) != btn) {
+          btn = HIGH;
+          estado = 1;
+        }
+        break;
+      case 1:
+        hcsr04();
+        if(distancia < 10) {
+          Serial.print("cesta B");
+          HC12B.write(50);
+          tone(buzzer,NOTE_C3,1000/4);
+          noTone(buzzer);
+        }
+        else if(!digitalRead(switchbtn) && btn) {
+          btn = LOW;
+        } 
+        else if(digitalRead(switchbtn) != btn) {
+          btn = HIGH;
+          estado = 0;
+        }
+        break;
+    }
+}
+
+void hcsr04(){
+  digitalWrite(trig, LOW); //SETA O PINO 6 COM UM PULSO BAIXO "LOW"
+  delayMicroseconds(2); // DELAY DE 2 MICROSSEGUNDOS
+  digitalWrite(trig, HIGH); //SETA O PINO 6 COM PULSO ALTO "HIGH"
+  delayMicroseconds(10); // DELAY DE 10 MICROSSEGUNDOS
+  digitalWrite(trig, LOW); //SETA O PINO 6 COM PULSO BAIXO "LOW" NOVAMENTE
+  // FUNÇÃO RANGING, FAZ A CONVERSÃO DO TEMPO DE
+  //RESPOSTA DO ECHO EM CENTÍMETROS E ARMAZENA
+  //NA VARIÁVEL "distancia"
+  distancia = (ultrasonic.Ranging(CM)); // VARIÁVEL GLOBAL RECEBE O VALOR DA DISTÂNCIA MEDIDA
+}
---
+++ placar/placar/placar.ino
@@ -0,0 +1,38 @@
+//receptor
+#include<SoftwareSerial.h>
+#define tx 10
+#define rx 11
+#define pinledA 3
+#define pinledB 4
+
+bool ledA = false;
+bool ledB = false;
+int op = 48;//0
+
+SoftwareSerial HC12(tx,rx);
+
+void setup() {
+  HC12.begin(9600);
+  Serial.begin(9600);
+  pinMode(pinledA, OUTPUT);
+  pinMode(pinledB, OUTPUT);
+}
+
+void loop() {
+  while(HC12.available()) {
+    op = HC12.read();
+    digitalWrite(op-48+2,HIGH);
+    Serial.println(op);
+    switch(op) {
+      case 49:
+        ledA = HIGH;
+        ledB = LOW;
+        digitalWrite(pinledB,LOW);
+      case 50:
+        ledB = HIGH;
+        ledA = LOW;
+        digitalWrite(pinledA,LOW);
+    }
+    
+  }
+}
