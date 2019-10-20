0f66ea1c83193c4fec6812556c5dc5a9240bf7c6
lucastavaresuerj <lucastavaresuerj2016@gmail.com>
2019-10-20 18:24:18 -0300

    WTF esse switch

--- placar/placar/placar.ino
+++ placar/placar/placar.ino
@@ -5,8 +5,6 @@
 #define pinledA 3
 #define pinledB 4
 
-bool ledA = false;
-bool ledB = false;
 int op = 48;//0
 
 SoftwareSerial HC12(tx,rx);
@@ -21,20 +19,7 @@ void setup() {
 void loop() {
   while(HC12.available()) {
     op = HC12.read();
-    digitalWrite(op-48+2,HIGH);
-    Serial.println(op);
-    switch(op) {
-      case 49:
-        ledA = HIGH;
-        ledB = LOW;
-        digitalWrite(pinledB,LOW);
-        break;
-      case 50:
-        ledB = HIGH;
-        ledA = LOW;
-        digitalWrite(pinledA,LOW);
-        break;
-    }
-    
+    digitalWrite(pinledA, op==49);
+    digitalWrite(pinledB, op==50);
   }
 }
